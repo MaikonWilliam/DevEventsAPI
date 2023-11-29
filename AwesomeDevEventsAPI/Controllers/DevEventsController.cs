89a64ed8736cdf280afddfe1abbd0a99d9f351d9
Maikon William <maikonwilliam1120@gmail.com>
2023-11-29 08:57:23 -0300

    Adicionar arquivos de projeto.

---
+++ AwesomeDevEventsAPI/Controllers/DevEventsController.cs
@@ -0,0 +1,83 @@
+﻿using AwesomeDevEventsAPI.Entities;
+using AwesomeDevEventsAPI.Persistence;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AwesomeDevEventsAPI.Controllers
+{
+    [Route("api/dev-events")]
+    [ApiController]
+    public class DevEventsController : ControllerBase
+    {
+        private readonly DevEventsDbContext _context;
+        public DevEventsController(DevEventsDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var devEvents = _context.DevEvents.Where(o => !o.IsDeleted).ToList();
+            return Ok(devEvents);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(Guid id)
+        {
+            var devEvent = _context.DevEvents.SingleOrDefault(e => e.Id == id);
+            if (devEvent == null)
+            {
+                return NotFound();
+            }
+            return Ok(devEvent);
+        }
+
+        [HttpPost]
+        public IActionResult Post(DevEvent devEvent)
+        {
+            _context.DevEvents.Add(devEvent);
+
+            return CreatedAtAction(nameof(GetById), new { id = devEvent.Id }, devEvent);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(Guid id, DevEvent input)
+        {
+            var devEvent = _context.DevEvents.SingleOrDefault(e => e.Id == id);
+            if (devEvent == null)
+            {
+                return NotFound();
+            }
+
+            devEvent.Update(input.Title, input.Description, input.StartDate, input.EndDate);
+            return NoContent();
+
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id)
+        {
+            var devEvent = _context.DevEvents.SingleOrDefault(e => e.Id == id);
+            if (devEvent == null)
+            {
+                return NotFound();
+            }
+            devEvent.Deleted();
+            return NoContent();
+        }
+
+        [HttpPost("{id}/speakers")]
+
+        public IActionResult PostSpeaker(DevEventSpeaker speaker,Guid id)
+        {
+            var devEvent = _context.DevEvents.SingleOrDefault(e => e.Id == id);
+            if (devEvent == null)
+            {
+                return NotFound();
+            }
+            devEvent.Speakers.Add(speaker);
+            return NoContent();
+        }
+    }
+}
